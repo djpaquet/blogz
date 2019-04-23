87027727fb5e5978349cdd162daa321912473d0e
Dana Paquet <danapaquet@gmail.com>
2019-04-23 00:30:59 -0500

    need to post to database

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Blog(db.Model):
     body = db.Column(db.String(400))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
 
         self.title = title
         self.body = body
@@ -25,29 +25,32 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    user = db.Column(db.String(120))
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, user, password):
+        self.user = user
         self.password = password
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'user' not in session:
         return redirect('/login')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    user = request.form['user']
+    password = request.form['password']
+    users = User.query.filter_by(user=user).first()
+    
+    
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        
         if user and user.password == password:
             #TODO "remember" that the user has logged in
-            session['email'] = email
+            session['user'] = user
             flash('Logged in')
             print(session)
             return redirect('/newpost')
@@ -60,63 +63,30 @@ def login():
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    
-    
+
     if request.method == 'POST':
-        email = request.form['email']
+        user = request.form['user']
         password = request.form['password']
         verify = request.form['verify']
-    
+        user_id = request.args.get('id')
+        #TODO - validate user data
 
-        #validate user data
-        #validate email
-        if not re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.])",email ):
-            email_error = 'Please enter a valid email'
-            email = ''
-
-        #validate password and verify password 
-        password_error = ''
-        if len(password) >=3 and len(password) <=20:
-            for character in password:
-                if character == ' ':
-                    password_error = 'Please enter a valid Password that has no spaces'
-                    password = ''
-        elif len(password) < 3 or len(password) > 20:
-            password_error = 'Password must be at least 6 characters'
-            password = ''
-        
-        verify_password_error = ''
-        if verify != password:
-            verify_password_error = 'Passwords do not match'
-            password = ''
-        return render_template ('signup.html', password=password, 
-                            password_error=password_error,
-                            verify=verify,
-                            verify_password_error=verify_password_error,
-                            email=email, email_error=email_error)
-
-        existing_user = User.query.filter_by(email=email).first()
+        existing_user = User.query.filter_by(user_id=user_id).first()
         if not existing_user:
-            new_user = User(email, password)
+            new_user = User(user, password)
             db.session.add(new_user)
             db.session.commit()
-            session['email'] = email
+            session['user'] = user
             #TODO remember the data
 
-            return redirect('/newpost')
+            return redirect('/')
         
-        #else:
+        else:
             #TODO user better response messaging
-            #return '<h1>Duplicate use</h1>'
+            return '<h1>Duplicate use</h1>'
         
             
-    return render_template ('signup.html')
-
-@app.route('/logout')
-def logout():
-    del session['email']
-    return redirect('/')
-
+    return render_template ('register.html')
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
     id = request.args.get('id')
@@ -131,9 +101,6 @@ def blog():
         return render_template('blog.html', title='Build a Blog', blogs=blogs)
 
 
-
-
-    
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     title_error = ""
@@ -142,7 +109,7 @@ def newpost():
         title = request.form['title']
         body = request.form['body']
         blog = Blog(title, body) 
-        #error = request.args.get('error')
+        error = request.args.get('error')
         
         if not title and not body:
             title_error = "Please enter a title for your blog entry"
@@ -159,10 +126,8 @@ def newpost():
             db.session.commit()
             return render_template('display_blog.html', title=title, body=body, blog=blog)
                 
+    return render_template('newpost.html')
+
     
-
-    return render_template('newpost.html', body_error=body_error, title_error=title_error)
-
-
 if __name__ == '__main__':
     app.run()
